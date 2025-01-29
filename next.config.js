c1d7013aede4709d6f4930d431ab81b031d11c51
Felipe Macedo <99462868+Zuk3s@users.noreply.github.com>
2025-01-29 19:45:44 -0300

    chore: add remote image domain to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https", // Protocolo usado
+        hostname: "image.tmdb.org", // Domínio permitido
+        port: "", // Porta (geralmente vazio para https)
+        pathname: "/t/p/**", // Caminho permitido com wildcard
+      },
+    ], // Adicione o domínio aqui
+  },
+};
 
 module.exports = nextConfig;
