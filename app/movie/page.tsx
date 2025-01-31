9362a5dc35297fd7c2130005e9f0c4d4927f0405
Felipe Macedo <99462868+Zuk3s@users.noreply.github.com>
2025-01-30 22:24:08 -0300

    feat(movie/pages): add server-side movies page.tsx

--- app/movie/page.tsx
+++ app/movie/page.tsx
@@ -1,3 +1,24 @@
-export default function MoviesPage() {
-  return <div>Movies</div>;
+import { fetchMovies } from "../api/movies";
+import { fetchGenres } from "../api/genres";
+import MovieContent from "./client-side";
+
+async function getData() {
+  const [initialMovies, initialGenres] = await Promise.all([
+    fetchMovies({}),
+    fetchGenres(),
+  ]);
+
+  return {
+    props: { initialGenres, initialMovies },
+  };
+}
+
+export default async function MoviesPage() {
+  const {
+    props: { initialGenres, initialMovies },
+  } = await getData();
+
+  return (
+    <MovieContent initialMovies={initialMovies} initialGenres={initialGenres} />
+  );
 }
