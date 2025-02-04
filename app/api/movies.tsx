44c252e0b8b77ef3f01c0142e85ae55e8b0ce320
Felipe Macedo <99462868+Zuk3s@users.noreply.github.com>
2025-02-04 00:37:54 -0300

    feat(api): update fetchMovieDetails to include additional data in response

--- app/api/movies.tsx
+++ app/api/movies.tsx
@@ -4,7 +4,6 @@ import {
   MovieDetailsResponse,
   MovieResponse,
   MoviesRequest,
-  VideoResponse,
 } from "@/types";
 import api from ".";
 
@@ -19,16 +18,21 @@ export async function fetchMovies({
   return response.data;
 }
 
+{
+  /*
+  - Adicionar o APPEND_TO_RESPOSNE, com as propriedades, "similar,recomendations,videos,release_date", ele precisa ser passado como query params
+  - Após isso, apague o fetchVideoMovie, pois ele não será mais necessário. 
+  - Também mude o Type Response dessa requisição.
+*/
+}
 export async function fetchMovieDetails(
   id: string
 ): Promise<MovieDetailsResponse> {
-  const response = await api.get(`/movie/${id}`);
-
-  return response.data;
-}
-
-export async function fetchVideosMovie(id: string): Promise<VideoResponse> {
-  const response = await api.get(`/movie/${id}/videos`);
+  const response = await api.get(`/movie/${id}`, {
+    params: {
+      append_to_response: "similar,recommendations,videos,release_dates",
+    },
+  });
 
   return response.data;
 }
