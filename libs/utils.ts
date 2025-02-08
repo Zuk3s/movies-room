75a43a64a9a7e97ca74457822c6c0bf2b5f63d34
Felipe Macedo <99462868+Zuk3s@users.noreply.github.com>
2025-02-08 00:40:27 -0300

    feat(utils): adicionar funções para formatar datas e duração de filmes

---
+++ libs/utils.ts
@@ -0,0 +1,14 @@
+export async function formatDate(dateString: string) {
+  const date = new Date(dateString);
+  return new Intl.DateTimeFormat("pt-BR", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  }).format(date);
+}
+
+export async function formatRuntime(minutes: number) {
+  const hours = Math.floor(minutes / 60);
+  const remainingMinutes = minutes % 60;
+  return `${hours}h ${remainingMinutes}m`;
+}
