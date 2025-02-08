152f75da7e74927d46e64da9875d2b9f01942d7b
Felipe Macedo <99462868+Zuk3s@users.noreply.github.com>
2025-02-08 00:37:08 -0300

    refactor: remover componentes não utilizados do projeto

--- app/lenis.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import ReactLenis from "@studio-freight/react-lenis";
-
-export function LenisScroll({ children }: { children: React.ReactNode }) {
-  return <ReactLenis root>{children}</ReactLenis>;
-}
