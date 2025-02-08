993d9482a06ec8868313a0c7b49de2f2d07458e9
Felipe Macedo <99462868+Zuk3s@users.noreply.github.com>
2025-02-08 00:39:46 -0300

    feat(genres): adicionar componente GenresList para exibir gêneros de filmes

---
+++ components/ui/details/genres/GenresList.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { GenreResponse } from "@/types";
+import { Chip } from "@heroui/react";
+
+export function GenresList({ genres }: { genres: GenreResponse[] }) {
+  return (
+    <div className="flex flex-wrap justify-center md:justify-start gap-1 sm:gap-2">
+      {genres.map((genre) => (
+        <Chip
+          key={genre.id}
+          variant="flat"
+          color="secondary"
+          className=" py-4 text-sm sm:text-base md:px-4 md:py-5 md:text-lg"
+        >
+          {genre.name}
+        </Chip>
+      ))}
+    </div>
+  );
+}
