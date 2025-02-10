5fe49b21ff9ed4782d5d3d7e37937aac0a7990a0
Felipe Macedo <99462868+Zuk3s@users.noreply.github.com>
2025-02-10 00:15:23 -0300

    feat(search-modal): adicionar modal de busca para pesquisa de filmes

---
+++ components/ui/SearchModal/SearchModal.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  Button,
+  useDisclosure,
+  Input,
+  Divider,
+} from "@heroui/react";
+import { SearchIcon } from "@/components/icons";
+import { Kbd } from "@heroui/kbd";
+import { useSearchMovies } from "@/libs/hooks/useSearchModal";
+import { Listbox, ListboxItem, ListboxSection } from "@heroui/listbox";
+
+export function SearchModal() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [searchQuery, setSearchQuery] = useState("");
+  const { movies, error } = useSearchMovies(searchQuery);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "k" && (event.metaKey || event.ctrlKey)) {
+        event.preventDefault();
+        onOpen();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [onOpen]);
+
+  return (
+    <>
+      <Button
+        onPress={onOpen}
+        aria-label="Search"
+        startContent={
+          <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+        }
+        endContent={
+          <Kbd className="hidden lg:inline-block" keys={["ctrl"]}>
+            Ctrl + K
+          </Kbd>
+        }
+        className="bg-default-700/10 hover:bg-default-700/20 "
+        type="button"
+      >
+        Search
+      </Button>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        size="2xl"
+        scrollBehavior="inside"
+        classNames={{
+          closeButton: "hidden",
+          base: ["bg-background", "outline outline-1 outline-white/20"],
+        }}
+      >
+        <ModalContent>
+          <ModalHeader className="flex flex-col gap-1">
+            <Input
+              autoFocus
+              placeholder="Procure por filmes..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              classNames={{
+                input: [
+                  "bg-transparent",
+                  "text-default-200",
+                  "text-lg",
+                  "placeholder:text-default-500",
+                ],
+                inputWrapper: [
+                  "bg-transparent",
+                  "backdrop-blur-sm",
+                  "data-[hover=true]:bg-transparent",
+                  "group-data-[focus=true]:bg-transparent",
+                ],
+              }}
+            />
+          </ModalHeader>
+          <Divider />
+          <ModalBody>
+            <Listbox variant="bordered" color="secondary">
+              {movies && (
+                <ListboxSection
+                  title={searchQuery ? "Resultados" : "Tendências"}
+                >
+                  {movies.map((movie) => (
+                    <ListboxItem href={`/movie/${movie.id}`} key={movie.id}>
+                      {movie.title}
+                    </ListboxItem>
+                  ))}
+                </ListboxSection>
+              )}
+            </Listbox>
+            {error && <p>Error: {error}</p>}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
---
+++ libs/hooks/useSearchModal.ts
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import type { MovieResponse } from "@/types";
+import { fetchMovieQuery, fetchTrendingMovies } from "@/app/api/movies";
+
+export function useSearchMovies(query: string) {
+  const [movies, setMovies] = useState<MovieResponse["results"]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      setError(null);
+
+      try {
+        let data: MovieResponse;
+        if (query) {
+          data = await fetchMovieQuery({ query });
+        } else {
+          data = await fetchTrendingMovies();
+        }
+        setMovies(data.results);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : "An error occurred");
+      }
+    };
+
+    const debounceTimer = setTimeout(fetchMovies, 300);
+
+    return () => clearTimeout(debounceTimer);
+  }, [query]);
+
+  return { movies, error };
+}
