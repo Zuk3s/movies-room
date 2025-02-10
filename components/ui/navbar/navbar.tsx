19bd81b44cb7544d6a4e9ada04f14b531a5d5a57
Felipe Macedo <99462868+Zuk3s@users.noreply.github.com>
2025-02-10 00:17:08 -0300

    feat(navbar): substituir campo de busca por modal de busca no componente Navbar

--- components/ui/navbar/navbar.tsx
+++ components/ui/navbar/navbar.tsx
@@ -7,9 +7,7 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@heroui/navbar";
-import { Kbd } from "@heroui/kbd";
 import { Link } from "@heroui/link";
-import { Input } from "@heroui/input";
 import { link as linkStyles } from "@heroui/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
@@ -19,37 +17,12 @@ import { ThemeSwitch } from "@/components/theme-switch";
 import {
   GithubIcon,
   DiscordIcon,
-  SearchIcon,
   Logo,
   LinkedinIcon,
 } from "@/components/icons";
+import { SearchModal } from "@/components/ui/SearchModal/SearchModal";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: [
-          "bg-default-700/10",
-          "group-data-[focus=true]:bg-default-700/20",
-          "data-[hover=true]:bg-default-700/20",
-        ],
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
     <HeroUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -97,7 +70,9 @@ export const Navbar = () => {
           </Link>
           <ThemeSwitch />
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
+        <NavbarItem className="hidden lg:flex">
+          <SearchModal />
+        </NavbarItem>
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
@@ -109,7 +84,7 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
+        <SearchModal />
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
